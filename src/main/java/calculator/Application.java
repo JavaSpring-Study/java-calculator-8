9ead665ca263d549bcbec5471972669fa28e8eed
Eunsaem03 <eunsaem910@gmail.com>
2025-10-20 01:31:35 +0900

    feat: 잘못된 값 입력 시 IllegalArgumentException 발생 및 종료 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,10 +3,15 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String input = InputView.readInput();
-        String[] splitInput = StringSplitter.splitInput(input);
-        int[] numbers = NumberParser.changeNumber(splitInput);
-        int result = SumCalculator.sum(numbers);
-        OutputView.printResult(result);
+        try {
+            String input = InputView.readInput();
+            String[] splitInput = StringSplitter.splitInput(input);
+            int[] numbers = NumberParser.changeNumber(splitInput);
+            int result = SumCalculator.sum(numbers);
+            OutputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error " + e.getMessage());
+            throw e;
+        }
     }
 }
--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -4,6 +4,9 @@ public class NumberParser {
     public static int[] changeNumber(String[] splitInput) {
         int[] numbers = new int[splitInput.length];
         for (int i = 0; i < splitInput.length; i++) {
+            if (!splitInput[i].matches("\\d+")) {
+                throw new IllegalArgumentException("형식을 벗어남");
+            }
             numbers[i] = Integer.valueOf(splitInput[i]);
         }
         return numbers;
