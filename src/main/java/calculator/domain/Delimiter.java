b6bb97416878350d2a7ac9ee1638a5e54e626af3
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-10-20 16:03:44 +0900

    fix: 예외처리 추가

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -6,17 +6,18 @@ import java.util.regex.Pattern;
 import calculator.message.ErrorMessage;
 
 public class Delimiter {
-	private final String regex;
-	private final String pattern = "//(.*)\\\\n(.*)";
+	private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.*?)\\\\?n(.*)", Pattern.DOTALL);
 
-	private Delimiter (String regax) {
-		this.regex = regax;
+	private final String regex;
+
+	private Delimiter (String regex) {
+		this.regex = regex;
 	}
 	public static Delimiter of(String value) {
-		Pattern p = Pattern.compile("//(.*)\\\\\\\\n(.*)",Pattern.DOTALL);
-		Matcher m = p.matcher(value);
-		if (m.matches()) {
-			String custom = m.group(1);
+		Matcher matcher = CUSTOM_PATTERN.matcher(value);
+
+		if (matcher.matches()) {
+			String custom = matcher.group(1);
 			if(custom.isEmpty()) { //커스텀 구분자가 빈 문자열이면 예외처리
 				throw new IllegalArgumentException(ErrorMessage.DELIMITER_MISSING);
 			}
@@ -25,8 +26,8 @@ public class Delimiter {
 		return new Delimiter("[,\n]");
 	}
 	public String extractNumbers(String value) {
-		Matcher m = Pattern.compile(pattern, Pattern.DOTALL).matcher(value);
-		return m.matches() ? m.group(2) : value;
+		Matcher matcher = CUSTOM_PATTERN.matcher(value);
+		return matcher.matches() ? matcher.group(2) : value;
 	}
 	public String getRegex() {
 		return regex;
--- src/main/java/calculator/domain/Separator.java
+++ src/main/java/calculator/domain/Separator.java
@@ -11,15 +11,20 @@ public class Separator {
 	private final List<Integer> numbers;
 
 	public Separator(String value) {
-			this.delimiter = Delimiter.of(value);
-			this.numbers = parseNumbers(delimiter.extractNumbers(value));
+			this.delimiter = Delimiter.of(value);//구분자 객체 생성
+			this.numbers = parseNumbers(delimiter.extractNumbers(value)); //숫자 변환
 	}
 
 	private List<Integer> parseNumbers(String nums) {
 		List<Integer> result = new ArrayList<>();
 		for (String token: nums.split(delimiter.getRegex())){
 			if(!token.isEmpty()) {
-				int num = Integer.parseInt(token);
+				int num;
+				try{
+					num = Integer.parseInt(token);
+				}catch (NumberFormatException e){ //숫자가 아닌 경우 예외처리
+					throw new IllegalArgumentException(ErrorMessage.ENTERED_NON_NUMERIC_VALUE);
+				}
 				//음수인경우 예외처리
 				if (num < 0) {
 					throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER_NOT_ALLOWED);
