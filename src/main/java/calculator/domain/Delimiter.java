abb6742564b712a98cf5324ab8ac7b73b64132db
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-10-20 21:33:39 +0900

    refactor: Delimiter 클래스 커스텀 구분자 처리 로직 개선

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 import calculator.message.ErrorMessage;
 
 public class Delimiter {
-	private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.*?)\\\\?n(.*)", Pattern.DOTALL);
+	private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.*?)\\\\n(.*) | //(.*?)\\n(.*)", Pattern.DOTALL); //group(1) / (2) : 커스텀 / 숫자 , group(3) / (4) : 커스텀 / 숫자
 
 	private final String regex;
 
@@ -17,17 +17,21 @@ public class Delimiter {
 		Matcher matcher = CUSTOM_PATTERN.matcher(value);
 
 		if (matcher.matches()) {
-			String custom = matcher.group(1);
-			if(custom.isEmpty()) { //커스텀 구분자가 빈 문자열이면 예외처리
+			String custom = matcher.group(1) != null ? matcher.group(1) : matcher.group(3);
+			if(custom == null || custom.isEmpty()) { //커스텀 구분자가 빈 문자열이면 예외처리
 				throw new IllegalArgumentException(ErrorMessage.DELIMITER_MISSING);
 			}
 			return new Delimiter(Pattern.quote(custom));
 		}
 		return new Delimiter("[,\n]");
 	}
+	//구분자 이후 숫자 문자열만
 	public String extractNumbers(String value) {
 		Matcher matcher = CUSTOM_PATTERN.matcher(value);
-		return matcher.matches() ? matcher.group(2) : value;
+		if (matcher.matches()) {
+			return matcher.group(2) != null ? matcher.group(2) : matcher.group(4);
+		}
+		return value;
 	}
 	public String getRegex() {
 		return regex;
