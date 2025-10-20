5fbdeae94db482eac0a051f25125d359b2ffebde
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-10-20 15:08:42 +0900

    refactor: 숫자 파싱 및 검증 로직을 Separator 클래스로 분리

--- src/main/java/calculator/domain/Separator.java
+++ src/main/java/calculator/domain/Separator.java
@@ -2,56 +2,34 @@ package calculator.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import calculator.message.ErrorMessage;
 
 public class Separator {
 
+	private final Delimiter delimiter;
 	private final List<Integer> numbers;
 
 	public Separator(String value) {
-		this.numbers = split(value);
+			this.delimiter = Delimiter.of(value);
+			this.numbers = parseNumbers(delimiter.extractNumbers(value));
 	}
 
-	public List<Integer> split(String value) {
-		String delimiter = "[,\n]"; // 기본 구분자 ,와 개행
-		String numbersPart = value;
-
-		// 커스텀 구분자 처리
-		Pattern pattern = Pattern.compile("//(.*)\\\\n(.*)", Pattern.DOTALL);
-		Matcher matcher = pattern.matcher(value);
-
-		if (matcher.matches()) {
-			String customDelimiter = matcher.group(1);
-			if (customDelimiter.isEmpty()) {
-				throw new IllegalArgumentException(ErrorMessage.DELIMITER_MISSING);
-			}
-			delimiter = Pattern.quote(customDelimiter);
-			numbersPart = matcher.group(2); // \n 뒤 숫자 문자열
-		}
-
-		String[] tokens = numbersPart.split(delimiter);
-
+	private List<Integer> parseNumbers(String nums) {
 		List<Integer> result = new ArrayList<>();
-		for (String token : tokens) {
-			if (!token.isEmpty()) {
-				try {
-					int num = Integer.parseInt(token);
-					if (num < 0) {
-						throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER_NOT_ALLOWED);
-					}
-					result.add(num);
-				} catch (NumberFormatException e) {
-					throw new IllegalArgumentException(ErrorMessage.ENTERED_NON_NUMERIC_VALUE);
+		for (String token: nums.split(delimiter.getRegex())){
+			if(!token.isEmpty()) {
+				int num = Integer.parseInt(token);
+				//음수인경우 예외처리
+				if (num < 0) {
+					throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER_NOT_ALLOWED);
 				}
+				result.add(num);
 			}
 		}
 		return result;
 	}
-
-	public List<Integer> getNumbers() {
+	public List<Integer> getNumbers(){
 		return numbers;
 	}
 }
